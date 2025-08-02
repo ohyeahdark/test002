71c6ee1d117ae9f4f1af74915b3c2e70b301daff
itaiua <hung.nguyen@tacorp.vn>
2025-08-02 11:06:38 +0700

    Update Header.tsx

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -173,7 +173,7 @@ const Header: React.FC<HeaderProps> = ({ onClick, onToggle }) => {
           } items-center justify-between w-full gap-4 px-5 py-4 lg:flex shadow-theme-md lg:justify-end lg:px-0 lg:shadow-none`}
         >
           <div className="flex items-center gap-2 2xsm:gap-3">
-            
+            <LanguageSwitcher /> 
             {/* <!-- Dark Mode Toggler --> */}
             <ThemeToggleButton />
             {/* <!-- Dark Mode Toggler --> */}
